89f8ca404a62d1375f2166377e40c0947eacd04a
MwSpace llc <app@mwspace.com>
2022-07-22 14:09:44 +0200

    update |

--- pages/api/shipments.js
+++ pages/api/shipments.js
@@ -1,5 +1,7 @@
 import {getAppSettings} from "../../lib/helpers";
 import {Carrier, Packlink, Stat} from "../../node_packages/packlink-js";
+import PostalZone from "../../node_packages/packlink-js/src/Models/PostalZone";
+import PostalCode from "../../node_packages/packlink-js/src/Models/PostalCode";
 
 export default async function handler(req, res) {
 
@@ -13,7 +15,9 @@ export default async function handler(req, res) {
 
     try {
 
-        // const carriers = Carrier.quote(3)
+        /**
+         * Carrier test | quote */
+        // const carriers = await Carrier.quote(50)
         //
         // carriers.from({
         //     country: 'IT',
@@ -27,9 +31,47 @@ export default async function handler(req, res) {
         //
         // return res.json(await carriers.all())
 
-        const stats = await Stat.all()
+        /**
+         * Carrier test | quote */
+        // const packages = [[]];
+        //
+        // const carriers = await Carrier.ship(packages)
+        //
+        // carriers.from({
+        //     country: 'IT',
+        //     zip: '20900'
+        // })
+        //
+        // carriers.to({
+        //     country: 'IT',
+        //     zip: '06073'
+        // });
+        //
+        // return res.json(await carriers.all())
 
-        return res.json(stats)
+        /**
+         *
+         * Stats test
+         */
+        // const stats = await Stat.all()
+
+        // return res.json(stats)
+
+        /**
+         *
+         * PostalZone test
+         */
+        // const postalzones = await PostalZone.all()
+        //
+        // return res.json(postalzones)
+
+        /**
+         *
+         * PostalCode test
+         */
+        const postalcodes = await PostalCode.all()
+
+        return res.json(postalcodes)
 
     } catch (e) {
 
